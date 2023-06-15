45824f1ca78238e64a976e5c792c0574cf6e54b4
Eric86y <eric.werner.mail@gmail.com>
2023-06-15 12:13:33 +0200

    added parameterized export formats

--- run_easter_inference.py
+++ run_easter_inference.py
@@ -8,9 +8,7 @@ from Lib.Modules import InferencePipeline
 
 """
 to run the pipeline, use e.g.:
-python run_easter_inference.py --input_dir "Output" --iiif_manifest "https://iiifpres.bdrc.io/vo:bdr:I1KG81132/manifest" --line_model "Models\LineModels\khyentse_wangpo_q.onnx" --ocr_model "Models\OCRModels\khyentse_wangpo_easter.hdf5"
-
-
+python run_easter_inference.py --input_dir "Output" --iiif_manifest "https://iiifpres.bdrc.io/vo:bdr:I1KG81132/manifest" --line_model "Models\LineModels\khyentse_wangpo_q.onnx" --ocr_model "Models\OCRModels\khyentse_wangpo_easter.hdf5" --export_formats "xml,text"
 """
 
 
@@ -22,10 +20,11 @@ if __name__ == "__main__":
     parser.add_argument("--ocr_model", type=str, required=True)
     parser.add_argument("--input_dir", type=str, required=True)
     parser.add_argument("--iiif_manifest", type=str, required=False)
+    parser.add_argument("--export_formats", type=str, required=False)
 
     args = parser.parse_args()
 
-    if os.path.isfile(args.line_model) and os.path.isfile(args.ocr_model):
+    if os.path.isfile(args.line_model) and os.path.isfile( args.ocr_model):
         if args.iiif_manifest:
             logging.debug("running inference from IIIF manifest..")
             iif_downloader = IIIFDownloader(output_dir=args.input_dir)
@@ -40,7 +39,11 @@ if __name__ == "__main__":
             model_characters=model_characters,
         )
 
-        pipeline.run(input_dir, output_formats=["xml", "prodigy", "text"])
+        if len(args.export_formats) > 0:
+            output_formats = args.export_formats.split(",")
+            print(output_formats)
+            
+        pipeline.run(input_dir, output_formats=output_formats)
 
     else:
         logging.info("No valid model paths were provided..")
