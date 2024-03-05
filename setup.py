5771a40f6b60e1615638f8c45379750c7e79cfd1
Eric86y <eric.werner.mail@gmail.com>
2024-03-05 13:56:20 +0100

    updated dependencies

--- setup.py
+++ setup.py
@@ -7,10 +7,12 @@ setup(
     author_email='eric.werner.mail@gmail.com',
     packages=['TibetanOCR'],
     install_requires=[
+        "fast-ctc-decode=>0.3.5",
         "numpy>=1.26",
+        "onnxruntime>=1.17",
         "opencv-python>=4.9",
-        "tqdm=>4.66",
-        "onnxruntime>=1.17"
+        "pyewts",
+        "tqdm=>4.66"  
     ],
     version='0.1',
     license='MIT',
